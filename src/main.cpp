e770a469b235b8562c407247f519c500c497bb4b
Y2J2142 <Y2J2142@hotmail.com>
2022-06-13 16:40:51 +0200

    Create basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,13 @@
-int main(){}
+#include <SFML/Window.hpp>
+int main() {
+	auto window = sf::Window{sf::VideoMode{800,600}, "Assteroids"};
+	while(window.isOpen()) {
+		sf::Event event;
+		while(window.pollEvent(event)) {
+			if(event.type == sf::Event::Closed)
+				window.close();
+		}
+	}
+
+	return 0;
+}
